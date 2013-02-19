125e1d4b2cb02ca3ff60f2997d2721cce5c97b96
Nathan Simeon <nsime298@gmail.com>
2013-02-19 09:02:27 -0500

    Inventory Manager and Scan Push

---
+++ week2/h5bp-html5-boilerplate-3cd1967/js/main.js
@@ -0,0 +1,38 @@
+// JavaScript Document
+
+
+
+var call_out;
+
+var awesomeFunction;
+
+var foo_bar;
+
+
+
+var thisisamodalalert;//bad function name!
+var This_is_a_modal_alert;
+var m;
+
+var callOut;
+callOut = "Hello World";
+//alert(callOut);
+console.log(callOut);
+
+var firstName;
+firstName = function(name){
+	//var name = "Nate";
+	console.log("How are you today, " + name + "?");
+	};
+
+firstName("Bob");
+firstName("Andy");
+firstName("Mike");
+
+var lastName;
+lastName = function(name){
+console.log(name);	
+};
+
+lastName("Simeon");
+
